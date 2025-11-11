e8b4979ca7e6720dae45f21755beee7e9b9e9fa2
papalino456 <sebastian.barrio.b@gmail.com>
2025-11-10 19:35:04 -0600

    fix motors

--- robot/tests/manual_tests/slam_navigation_triangle_test.py
+++ robot/tests/manual_tests/slam_navigation_triangle_test.py
@@ -88,7 +88,7 @@ from src.slam.slam_manager import SlamManager
 # -----------------------------
 
 # Motor/encoder wiring from motor_encoder_test.py
-MOTOR_CHANNELS: Tuple[Tuple[int, int], ...] = ((0, 1), (2, 3), (4, 5), (6, 7))
+MOTOR_CHANNELS: Tuple[Tuple[int, int], ...] = ((1, 0), (3, 2), (5, 4), (7, 6))
 ENCODER_PINS: Tuple[Tuple[int, int], ...] = ((5, 6), (13, 26))  # two encoders
 EXPECTED_MOTOR_TO_ENCODER: Dict[int, int] = {0: 0, 1: 0, 2: 1, 3: 1}
 ENCODER_PPR: int = 400
